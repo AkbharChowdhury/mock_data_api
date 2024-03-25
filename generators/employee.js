af5b0ef922572766cd76680f8c2fd6d2f4aa7ce3
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2024-03-25 21:38:54 +0000

    d

--- generators/employee.js
+++ generators/employee.js
@@ -6,6 +6,15 @@ module.exports = () => {
         name: chance.name({ nationality: 'en'}),
         occupation: chance.profession({ rank: true }),
         salary: chance.dollar({ min: 15_000, max: 150_000}),
+        age: chance.age({ type: 'adult' }),
+        address:{
+            address: chance.address(),
+            city: chance.city(),
+            country: chance.country({ full: true }),
+
+            postcode: chance.postcode()
+
+        }
     }
 
 };
