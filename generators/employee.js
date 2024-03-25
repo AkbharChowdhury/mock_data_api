d872a4fabd73f99abced72d761e0e3df951d50e3
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2024-03-25 15:51:23 +0000

    c

--- generators/employee.js
+++ generators/employee.js
@@ -1,13 +1,5 @@
 const chance  = require('chance').Chance();
-// module.exports = () => {
-//     return {
-//         id: chance.integer({ min: 1_000, max: 9_999}),
-//         name: chance.name({ nationality: 'en'}),
-//         occupation: chance.profession({ rank: true }),
-//         salary: chance.dollar({ min: 15_000, max: 150_000}),
-//     }
 
-// };
 module.exports = () => {
     return {
         id: chance.integer({ min: 1_000, max: 9_999}),
--- index.js
+++ index.js
@@ -5,27 +5,19 @@ const app = express();
 const mockData = (request, response, generator) => {
 
     const count = request.query.count || 30;
-    const data = [];
-
+    // const data = [];
     // for (let i = 0; i < count; i++) {
     //     data.push(generator())
     // }
-
-
     // response.json(data);
-
-
     response.json({
         'employees': Array.from(Array(Number(count)).keys()).map(x =>  generator())
 
-    })
+    });
 
 }
 
 app.get('/employees', (request, response) => mockData(request, response, employee));
-
-
-
 app.listen(port, () => {
     console.log(`server running at ${port}`)
 })
