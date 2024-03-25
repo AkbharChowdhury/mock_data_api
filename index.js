6c949047c16de9091dbaca4bfe4b6235b90a8fec
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2024-03-25 15:54:24 +0000

    x

--- index.js
+++ index.js
@@ -3,19 +3,14 @@ const express = require("express");
 const employee = require("./generators/employee");
 const app = express();
 const mockData = (request, response, generator) => {
-
     const count = request.query.count || 30;
-    // const data = [];
-    // for (let i = 0; i < count; i++) {
-    //     data.push(generator())
-    // }
-    // response.json(data);
     response.json({
         'employees': Array.from(Array(Number(count)).keys()).map(x =>  generator())
 
     });
 
 }
+
 
 app.get('/employees', (request, response) => mockData(request, response, employee));
 app.listen(port, () => {
