6d4b81d7ffbfb826a8c3b54c55e7c4eeda3dc4c8
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2024-03-25 21:23:43 +0000

    d

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const port = 3_000;
 const express = require("express");
 const employee = require("./generators/employee");
 const app = express();
-const mockData = (request, response, generator) => {
-    const count = request.query.count || 30;
-    response.json({
-        'employees': Array.from(Array(Number(count)).keys()).map(i =>  generator())
 
-    });
-
+const log = (response, data) => {
+    response.json(data);
+    console.log(data)
 }
-app.get('/employees', (request, response) => mockData(request, response, employee));
-app.listen(port, () => {
-    console.log(`server running at ${port}`)
-})
+const mockData = (request, response, employee) => {
+    const count = Number(request.query.count) || 5;
+    const employees = Object.freeze({
+        'employees': Array.from({ length: count }, employee)
+    })
+    log(response, employees)
+}
+app.get('/employees', (request, response) =>  mockData(request, response, employee));
+app.listen(port, () => console.log(`server running at ${port}`))
 
 // http://localhost:3000/employees
