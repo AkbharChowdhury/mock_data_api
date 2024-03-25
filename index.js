b0f656ab8193b90eb2eb0a47036d85f1d4fef490
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2024-03-25 15:44:12 +0000

    x

--- index.js
+++ index.js
@@ -6,24 +6,28 @@ const mockData = (request, response, generator) => {
 
     const count = request.query.count || 30;
     const data = [];
+
     // for (let i = 0; i < count; i++) {
     //     data.push(generator())
     // }
-    for(let _ in count){
-        data.push(generator())
-    }
 
-  
 
-   
+    // response.json(data);
 
-    response.json(data);
+
+    response.json({
+        'employees': Array.from(Array(Number(count)).keys()).map(x =>  generator())
+
+    })
 
 }
 
 app.get('/employees', (request, response) => mockData(request, response, employee));
 
 
-app.listen(port, () => console.log(`server running at ${port}`))
+
+app.listen(port, () => {
+    console.log(`server running at ${port}`)
+})
 
 // http://localhost:3000/employees
