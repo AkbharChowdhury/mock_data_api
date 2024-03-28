02c9dba6dbcfd53d2bc2f8ccdef4fa4199843d5f
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2024-03-28 09:21:30 +0000

    d

--- index.js
+++ index.js
@@ -11,10 +11,9 @@ const mockData = (request, response, employee) => {
     const count = Number(request.query.count) || 5;
     const employees = Object.freeze({
         'employees': Array.from({ length: count }, employee)
-    })
-    log(response, employees)
+    });
+    log(response, employees);
 }
 app.get('/employees', (request, response) =>  mockData(request, response, employee));
 app.listen(port, () => console.log(`server running at ${port}`))
-
 // http://localhost:3000/employees
